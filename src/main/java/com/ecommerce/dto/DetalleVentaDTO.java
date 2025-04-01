06f20f4f18e2413e5da709cc48ef153f8f0a58cf
Marcelo Pauls <paulsmarcelo@live.com>
2025-04-01 00:16:30 -0300

    Se agregó que tambien se muestre los id producto y id categoria en detalle venta

--- src/main/java/com/ecommerce/dto/DetalleVentaDTO.java
+++ src/main/java/com/ecommerce/dto/DetalleVentaDTO.java
@@ -3,14 +3,18 @@ package com.ecommerce.dto;
 import com.ecommerce.model.DetalleVenta;
 
 public class DetalleVentaDTO {
+    public Long idProducto;
     public String nombreProducto;
+    public Long idCategoria;
     public String nombreCategoria;
     public int cantidad;
     public double precioUnitario;
     public double totalDetalle;
 
     public DetalleVentaDTO(DetalleVenta detalleVenta) {
+        this.idProducto = detalleVenta.getProducto().getIdProducto();
         this.nombreProducto = detalleVenta.getProducto().getNombre();
+        this.idCategoria = detalleVenta.getProducto().getCategoria().getIdCategoria();
         this.nombreCategoria = detalleVenta.getProducto().getCategoria().getNombre();
         this.cantidad = detalleVenta.getCantidad();
         this.precioUnitario = detalleVenta.getProducto().getPrecioVenta();
